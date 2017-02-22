227c3569a9f13e6387332fb507ae575b9d5ae340
Mathias Ducancel <mathiasduc@gmail.com>
2017-02-22 12:54:01 +0100

    Add API route for generating paragraphs
    The route execute an instance of Faker to generate as requested

---
+++ app/Http/Controllers/LoremController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Faker\Factory as Faker;
+
+class LoremController extends Controller
+{
+	public function generateParagraphs($numberOfParagraphs){
+		$faker = Faker::create();
+    	return ($faker->paragraphs($numberOfParagraphs, true));
+	}	
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/generateParagraphs/paragraphs/{numberOfParagraphs}', 'LoremController@generateParagraphs')->name('faker');
