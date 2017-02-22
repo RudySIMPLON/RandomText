7b00cfcfbab41151695630e716c7915722a60190
simplon <rudy__10@hotmail.fr>
2017-02-22 16:52:29 +0100

    changement couleur body site responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,21 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.css">
 	<link rel="stylesheet" href="{{mix('/css/app.css')}}">
 </head>
 <body>
-	<header>Random texte</header>
 
+	<div class="titre">
+	<h1 >Lorem Easy</h1>
+	</div>
+
+ <div class="background">
 	<div class="head">
 
 		<div class="Mot">
 			<h3>nombre de mot</h3>
-			<input type="range" name="NbrMot" id="NbrMot"  class="NbrMot"
+			<input type="range" name="nbrMot" id="nbrMot"  class="nbrMot"
 			value="4" min="0" max="20">
 
 		</div>
@@ -20,26 +25,27 @@
 			<div >
 				<input type="checkbox" name="check-p" id="check-p" class="check-p">	
 				
-			</div>p
+			</div>p <br><br>
 			<div>
-				<input type="checkbox"textearea name="check-h2" id="check-h2" class="check-h2">	
+				<input type="checkbox" name="check-h2" id="check-h2" class="check-h2">	
 			</div>
-			 h3
+			h3
 		</div>
 
 		<div class="Paragraphe">
 			<h3>nombre de Paragraphe</h3>
-			<input type="range" name="NbrPara" id="NbrPara"  class="NbrPara"
+			<input type="range" name="nbrPara" id="nbrPara"  class="nbrPara"
 			value="4" min="0" max="20">
 		</div>
 	</div>
 
-	<div class="textarea">
-		<textarea name="textearea" classe="textearea" id="textearea" cols="80" rows="10"></textarea>
+	<div class="divTextarea">
+		<textarea name="textearea"  id="textearea" cols="80" rows="10"></textarea>
 	</div>
 
+</div>
 
-	<script scr="{mix('/js/app.js')}}"></script>
+	<script src="{{mix('/js/app.js')}}"></script>
 </body>
 
 </html>
