c5629b40b1ea9ff58c1545c1d93e217c7d9094b3
simplon <rudy__10@hotmail.fr>
2017-02-22 12:19:17 +0100

    routage creation vu

---
+++ resources/views/generateur.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="{{mix('/css/app.css')}}">
+</head>
+<body>
+	<header>Random texte</header>
+
+	<div class="head">
+
+		<div class="Mot">
+			<h3>nombre de mot</h3>
+			<input type="range" name="NbrMot" id="NbrMot"  class="NbrMot"
+			value="" min="0" max="20">
+
+		</div>
+		<div class="Html">
+			<div>
+				<input type="checkbox" name>	
+				
+			</div>p
+			<div>
+				<input type="checkbox">	
+			</div>
+			 h3
+		</div>
+
+		<div class="Paragraphe">
+			<h3>nombre de mot</h3>
+			<input type="range" name="NbrPara" id="NbrPara"  class="NbrPara"
+			value="" min="0" max="20">
+		</div>
+	</div>
+
+	<div class="textarea">
+		<textarea name="" id="" cols="50" rows="10"></textarea>
+	</div>
+
+
+
+	
+</body>
+</html>
