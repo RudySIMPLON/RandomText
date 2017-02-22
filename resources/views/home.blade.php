a3f791159cab86116fde71e68ea0af7fd9e58605
simplon <rudy__10@hotmail.fr>
2017-02-22 13:50:34 +0100

    modification class

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,29 +13,29 @@
 		<div class="Mot">
 			<h3>nombre de mot</h3>
 			<input type="range" name="NbrMot" id="NbrMot"  class="NbrMot"
-			value="" min="0" max="20">
+			value="4" min="0" max="20">
 
 		</div>
 		<div class="Html">
 			<div >
-				<input type="checkbox" name>	
+				<input type="checkbox" name="check-p" id="check-p" class="check-p">	
 				
 			</div>p
 			<div>
-				<input type="checkbox">	
+				<input type="checkbox"textearea name="check-h2" id="check-h2" class="check-h2">	
 			</div>
 			 h3
 		</div>
 
 		<div class="Paragraphe">
-			<h3>nombre de mot</h3>
+			<h3>nombre de Paragraphe</h3>
 			<input type="range" name="NbrPara" id="NbrPara"  class="NbrPara"
-			value="" min="0" max="20">
+			value="4" min="0" max="20">
 		</div>
 	</div>
 
 	<div class="textarea">
-		<textarea name="textearea" id="textearea" cols="80" rows="10"></textarea>
+		<textarea name="textearea" classe="textearea" id="textearea" cols="80" rows="10"></textarea>
 	</div>
 
 
